3b3b44f9cc7a58a470f096ef48ce2b21e432f707
YatinK <yatin.kare@gmail.com>
2025-05-18 11:14:26 -0400

    Basic changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
+<main>
+  <section
+    class="card bg-surface-100-900 rounded-container overflow-hidden"
+  ></section>
+</main>
